cc8aa678d060b422f92e1b21264883bcb297c36d
JoakoU <kawaidashxd@gmail.com>
2026-07-25 18:16:19 -0300

    v2.2.8

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", () => {
-  const posicion = window.scrollY;
-  const fondoSlow = posicion * 0.3;
-  document.body.style.backgroundPositionY = -200 + fondoSlow + "px";
-});
