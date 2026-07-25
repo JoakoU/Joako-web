4bb974b77ef84c44f4e2abb9e5ad3352c8570ffa
JoakoU <kawaidashxd@gmail.com>
2026-07-25 17:32:23 -0300

    v2.2.7: fix

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 window.addEventListener("scroll", () => {
   const posicion = window.scrollY;
-
-  const fondoSlow = posicion * 0.4;
-
-  document.body.style.backgroundPositionY = fondoSlow + "px";
+  const fondoSlow = posicion * 0.3;
+  document.body.style.backgroundPositionY = -200 + fondoSlow + "px";
 });
